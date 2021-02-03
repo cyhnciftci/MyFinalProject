109ebc45dc393958ef31100c540293a5768118a8
Sefer <Sefer@DESKTOP-5O93JGC>
2021-02-03 23:46:00 +0300

    Add project files.

---
+++ DataAccess/Abstract/IEntityRepository.cs
@@ -0,0 +1,18 @@
+﻿using Entities.Abstarct;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DataAccess.Abstract
+{
+    //generic cpnstraint
+    public interface IEntityRepository<T>where T:class,IEntity
+    {
+        List<T> GetAll(Expression<Func<T,bool>> filter=null);
+        T Get(Expression<Func<T, bool>> filter);
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+    }
+}
---
+++ Entities/Abstarct/IEntity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Abstarct
+{
+    //Ientity implement eden bir class veritabanı taplosudur
+    public interface IEntity
+    {
+    }
+}
